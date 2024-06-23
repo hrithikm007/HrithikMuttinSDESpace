87e299b007979647976a4ed8f4620cbe35c97f26
Hrithik <Hrithik007@gmail.com>
2024-06-23 23:57:55 +0530

    DTO

--- CodeBase/Spring Boot Microservices/Section2/accounts/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ CodeBase/Spring Boot Microservices/Section2/accounts/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,13 +1,23 @@
 package com.eazybytes.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
     @GetMapping("helloworld")
     public String getAccounts() {
         return "Hello World";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto())
     }
 }
---
+++ CodeBase/Spring Boot Microservices/Section2/accounts/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,11 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long account_number;
+    private String account_type;
+    private String branch_address;
+}
